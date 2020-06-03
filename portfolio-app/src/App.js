b5362e7ea547d371f84144903c6a5f3808de26ef
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-06-03 14:42:49 -0700

    completes prelim projects page

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Nav from "./Nav";
 import HomePage from "./HomePage";
+import Projects from "./Projects";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <HomePage />
+      <div className="home-container">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/projects" component={Projects} />
+          <Route component={HomePage} />
+        </Switch>
+      </div>
     </div>
   );
 }
