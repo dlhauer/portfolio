969a8de83ebd9425936d5432cdf1587eae8e73c8
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-06-05 10:44:52 -0700

    adds PR data

---
+++ portfolio-app/src/prs.js
@@ -0,0 +1,112 @@
+export default {
+  MyDish: {
+    description:
+      "Some select Pull Requests of mine on the React Native front end for MyDish.",
+    all:
+      "https://github.com/Lambda-School-Labs/mydish-fe/pulls?page=1&q=is%3Apr+is%3Aclosed+author%3Adlhauer",
+    select: [
+      {
+        id: 152,
+        title: "Feature/add suggested ingredients to form #152",
+        url: "https://github.com/Lambda-School-Labs/mydish-fe/pull/152",
+      },
+      {
+        id: 141,
+        title: "Feature/add suggested ingredients to form #141",
+        url: "https://github.com/Lambda-School-Labs/mydish-fe/pull/141",
+      },
+      {
+        id: 136,
+        title: "Feature/time input component #136",
+        url: "https://github.com/Lambda-School-Labs/mydish-fe/pull/136",
+      },
+      {
+        id: 131,
+        title: "Feature/version history view #131",
+        url: "https://github.com/Lambda-School-Labs/mydish-fe/pull/131",
+      },
+      {
+        id: 146,
+        title: "Fix/unhandled 400 error #146",
+        url: "https://github.com/Lambda-School-Labs/mydish-fe/pull/146",
+      },
+      {
+        id: 127,
+        title: "Styling/commit modal #127",
+        url: "https://github.com/Lambda-School-Labs/mydish-fe/pull/127",
+      },
+      {
+        id: 118,
+        title: "Feature/create recipe form for edit #118",
+        url: "https://github.com/Lambda-School-Labs/mydish-fe/pull/118",
+      },
+      {
+        id: 160,
+        title: "Feature/quantity length and keyboard #160",
+        url: "https://github.com/Lambda-School-Labs/mydish-fe/pull/160",
+      },
+      {
+        id: 159,
+        title: "Fix/pop individual recipe on blur #159",
+        url: "https://github.com/Lambda-School-Labs/mydish-fe/pull/159",
+      },
+      {
+        id: 154,
+        title: "Feature/ingredient prediction conditions #154",
+        url: "https://github.com/Lambda-School-Labs/mydish-fe/pull/154",
+      },
+      {
+        id: 71,
+        title: "Feature/image upload modal #71",
+        url: "https://github.com/Lambda-School-Labs/mydish-fe/pull/71",
+      },
+    ],
+  },
+  Knapsack: {
+    description:
+      "Some select Pull Requests of mine on the Flask back end for The Ant's Knapsack.",
+    all:
+      "https://github.com/Organization-for-CS-Build-Week-1/CS-Build-Week-1-Flask/pulls?q=is%3Apr+is%3Aclosed+author%3Adlhauer",
+    select: [
+      {
+        id: 2,
+        title: "Feature/generate room coords #2",
+        url:
+          "https://github.com/Organization-for-CS-Build-Week-1/CS-Build-Week-1-Flask/pull/2",
+      },
+      {
+        id: 3,
+        title: "Creates Room subclasses Tunnel and DeadEnd #3",
+        url:
+          "https://github.com/Organization-for-CS-Build-Week-1/CS-Build-Week-1-Flask/pull/3",
+      },
+      {
+        id: 6,
+        title: "Makes Walker randomness dynamic, saves map seed #6",
+        url:
+          "https://github.com/Organization-for-CS-Build-Week-1/CS-Build-Week-1-Flask/pull/6",
+      },
+      {
+        id: 10,
+        title: "Feature/room names and descriptions #10",
+        url:
+          "https://github.com/Organization-for-CS-Build-Week-1/CS-Build-Week-1-Flask/pull/10",
+      },
+      {
+        id: 22,
+        title: "Feature/store subclass #22",
+        url:
+          "https://github.com/Organization-for-CS-Build-Week-1/CS-Build-Week-1-Flask/pull/22",
+      },
+    ],
+  },
+  Sauti: {
+    description: `I read somewhere that if you're not embarrassed by your old work, then you're not learning enough.
+      Well, let's take these PRs as a case in point. Although I think the code I wrote is pretty good, you can see that
+      I still had a lot to learn about using branches and writing descriptive pull requests. My progress in that regard
+      is clear in subsequent projects.`,
+    all:
+      "https://github.com/Sauti-Studio3/Back-End/pulls?q=is%3Apr+is%3Aclosed",
+    select: [],
+  },
+};
