11fc4edd980c87871253929d26102e0f93088926
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-06-06 14:50:12 -0700

    adds rel addr

--- portfolio-app/src/PRList.js
+++ portfolio-app/src/PRList.js
@@ -8,16 +8,22 @@ const PRList = ({ project }) => (
         .sort((a, b) => b.id - a.id)
         .map((pr) => {
           return (
-            <a className="link pr-link" href={pr.url} target="_blank">
+            <a
+              className="link pr-link"
+              href={pr.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               {pr.title}
             </a>
           );
         })}
     </div>
     <a
-      className={`link ${!project.select && "project-link"}`}
+      className={`link ${!project.select && "project-link"} pr-link`}
       href={project.all}
       target="_blank"
+      rel="noopener noreferrer"
     >
       {`View all of my PRs for ${project.name}`}
     </a>
